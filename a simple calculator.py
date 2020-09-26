e2267cb5577488526c32f97f18fdfa99853bdaa2
ManasVerma1357 <71920045+ManasVerma1357@users.noreply.github.com>
2020-09-26 12:47:16 +0530

    Add files via upload

---
+++ a simple calculator.py
@@ -0,0 +1,11 @@
+'''21.9.20
+author-manas verma
+a simple calculator'''
+print("type your first number")
+n1 = input()
+print("type your second number")
+n2 = input()
+print("sum of the numbers is", int(n1)+int(n2))
+print("and the product of the numbers are", int(n1)*int(n2))
+print("the division of these numbers are", int(n1)/int(n2))
+#we have to put variable.i alwways not remember that hehe
